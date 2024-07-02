6be4eb455f42947a0d84efcf70c949e2a5ff76a6
Rakesh <rakeshvirat99@gmail.com>
2024-07-02 14:34:11 +0530

    fetchapi call

---
+++ Day03/BasicUI.dart
@@ -0,0 +1,3 @@
+// #HomeTask: API Integration using packages API and show Description in UI using Text widget
+// Reference  : https://dart.dev/tutorials/server/fetch-data
+//              https://dartpad.dev/?id&sample=counter
