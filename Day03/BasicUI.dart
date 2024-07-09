7eddc28b14ad70fa139b9b99ca5dc0f07afa9c60
Rakesh <rakeshvirat99@gmail.com>
2024-07-09 11:31:37 +0530

    updates

--- Day03/BasicUI.dart
+++ Day03/BasicUI.dart
@@ -1,3 +1,4 @@
 // #HomeTask: API Integration using packages API and show Description in UI using Text widget
 // Reference  : https://dart.dev/tutorials/server/fetch-data
 //              https://dartpad.dev/?id&sample=counter
+// Read: https://docs.flutter.dev/ui/widgets/basics
---
+++ state_work/lib/basic_form.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Form Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Form Demo'),
+        ),
+        body: MyCustomForm(),
+      ),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Enter your name'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your name';
+                }
+                return null;
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(SnackBar(content: Text('Processing Data')));
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
